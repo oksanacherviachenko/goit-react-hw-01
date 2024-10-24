ee11d263489635dc7c0721453364fb3dab435ffb
Oksana Cherviachenko <kvintinovna@gmail.com>
2024-10-24 21:29:21 +0200

    add homework-1 react

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// src/App.jsx
+
+import React from 'react';
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Social Network Profile, Friends, and Transactions</h1>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
+
+export default App;
+
+
---
+++ src/Components/FriendList/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './FriendList.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className={styles.friendItem}>
+      <img src={avatar} alt={name} width="48" className={styles.avatar} />
+      <p className={styles.name}>{name}</p>
+      <p className={isOnline ? styles.online : styles.offline}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
